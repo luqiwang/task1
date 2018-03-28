7e2d4096433f0dc0f1e497c83d1758b3d809623b
luqiwang <wangluqi1001@gmail.com>
2018-03-28 11:44:18 -0400

    add react-router

--- assets/js/Demo.jsx
+++ assets/js/Demo.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import _ from 'underscore';
+import Nav from './Nav';
+import $ from "jquery";
 
 export default function demo_init() {
   let root = document.getElementById('root');
@@ -12,12 +15,39 @@ class Demo extends Component {
     super(props);
     this.state = {
       tasks: [],
-      user: ""
-    }
+      user: [],
+    };
+    this.request_tasks();
+    this.request_users();
   }
   render() {
     return (
-      <div>React Loaded</div>
+      <Router>
+        <Nav />
+      </Router>
     )
+  }
+
+  request_tasks() {
+    $.ajax("/api/v1/tasks", {
+      method: "get",
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      success: (resp) => {
+        this.setState(_.extend(this.state, { posts: resp.data }));
+      },
+    });
+  }
+
+  request_users() {
+    $.ajax("/api/v1/users", {
+      method: "get",
+      dataType: "json",
+      contentType: "application/json; charset=UTF-8",
+      success: (resp) => {
+        console.log("api", resp)
+        this.setState(_.extend(this.state, { users: resp.data }));
+      },
+    });
   }
 }
--- assets/js/Nav.jsx
+++ assets/js/Nav.jsx
@@ -1,3 +1,25 @@
+// this part based on Professor Nat's Example: https://github.com/NatTuck/microblog-spa/blob/lec19-end/assets/js/cs/nav.jsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { NavItem } from 'reactstrap';
+
+export default function Nav(props) {
+  return (
+    <nav className="navbar navbar-dark bg-primary navbar-expand">
+      <span className="navbar-brand">
+        TaskTracker
+      </span>
+      <ul className="navbar-nav mr-auto">
+        <NavItem>
+          <NavLink to="/" exact={true} activeClassName="active" className="nav-link">Tasks</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/users" href="#" className="nav-link">All Users</NavLink>
+        </NavItem>
+      </ul>
+      <span className="navbar-text">
+        user@host
+      </span>
+    </nav>
+  );
+}
